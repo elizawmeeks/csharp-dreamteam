35860c5525a8aed2a91f0a7f594bc3d28386c9c5
Eliza Meeks <theartstudio@Elizas-MacBook-Pro.local>
2017-07-12 15:19:04 -0500

    made the thing

---
+++ Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dreamteam
+{
+    public interface ICohort {
+
+        string Specialty { get; set; }
+        string FirstName { get; set; }
+        string LastName{ get; set; }
+        string FullName { get; }
+       void Work();
+    }
+
+    class Eliza : ICohort
+    {
+        public string Specialty { get => "C#"; set => Specialty = value; }
+        public string FirstName { get => "Eliza"; set => FirstName = value; }
+        public string LastName { get => "Meeks"; set => LastName = value; }
+
+        public string FullName {get => FirstName + " " + LastName;}
+
+        public void Work()
+        {
+            Console.WriteLine("Eliza gets stuff done and is sometimes nice and sometimes fun.");
+        }
+    }
+
+    class Chaz : ICohort
+    {
+        public string Specialty { get => "C#"; set => Specialty = value; }
+        public string FirstName { get => "Chaz"; set => FirstName = value; }
+        public string LastName { get => "Henricks"; set => LastName = value; }
+
+        public string FullName {get => FirstName + " " + LastName;}
+
+        public void Work()
+        {
+            Console.WriteLine("Chaz is p helpful and wears lots of hats.");
+        }
+    }
+
+    public class Jackie : ICohort
+    {
+        public string Specialty { get => "C#"; set => Specialty = value; }
+        public string FirstName { get => "Jackie"; set => FirstName = value; }
+        public string LastName { get => "Knight"; set => LastName = value; }
+
+        public string FullName {get => FirstName + " " + LastName;}
+
+        public void Work()
+        {
+            Console.WriteLine("Jackie eats Cheetos for breakfast and chops hair. NEVER USE TABLES!!!");
+        }
+    }
+
+    public class Madeline : ICohort
+    {
+        public string Specialty { get => "CSS"; set => Specialty = value; }
+        public string FirstName { get => "Madeline"; set => FirstName = value; }
+        public string LastName { get => "Power"; set => LastName = value; }
+
+        public string FullName {get => FirstName + " " + LastName;}
+
+        public void Work()
+        {
+            Console.WriteLine("Madeline kicks ass and never brushes her hair.");
+        }
+    }
+
+    public class Kathy : ICohort
+    {
+        public string Specialty { get => "HTML"; set => Specialty = value; }
+        public string FirstName { get => "Kathy"; set => FirstName = value; }
+        public string LastName { get => "Weisensal"; set => LastName = value; }
+
+        public string FullName {get => FirstName + " " + LastName;}
+
+        public void Work()
+        {
+            Console.WriteLine("Kathy will have none of your shit and if you have a problem with that it is only your problem.");
+        }
+    }
+
+    public class Tamela : ICohort
+    {
+        public string Specialty { get => "HTML"; set => Specialty = value; }
+        public string FirstName { get => "Tamela"; set => FirstName = value; }
+        public string LastName { get => "Lerma"; set => LastName = value; }
+
+        public string FullName {get => FirstName + " " + LastName;}
+
+        public void Work()
+        {
+            Console.WriteLine("Tamela will support you as long as she can get some vodka and cigs in on the side.");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You got this, girl!!");
+
+            Eliza eliza = new Eliza();
+            Chaz chaz = new Chaz();
+            Jackie jackie = new Jackie();
+            Kathy kathy = new Kathy();
+            Tamela tamela = new Tamela();
+            Madeline madeline = new Madeline();
+
+            List<ICohort> serverSide = new List<ICohort>{
+                eliza, chaz, jackie
+            };
+
+            List<ICohort> clientSide = new List<ICohort>{
+                kathy, tamela, madeline
+            };
+
+            foreach (var gal in serverSide){
+                gal.Work();
+            }
+
+            foreach (var gal in clientSide){
+                gal.Work();
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # csharp-dreamteam
+
+NSS Exercise
+
+## Instructions
+
+1. Your job is to pick 5 of your teammates in your cohort and build a class for each one. Each teammate should have the following properties/methods. Build one for yourself, as well.
+```
+Specialty property - This holds the technology that the person enjoys the most.
+FirstName property
+LastName property
+FullName property - This property is a readonly property that returns the first and last name concatenated.
+Work() method - This will write a comical message to the console that describes the work they will do on a group project, based on their speciality.
+```
+1. Once you're done, you should have 6 different types in total, each with the properties and methods above.
